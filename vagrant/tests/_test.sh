ca5a156af242f375c743b222219c3fed8a85b0ef
Laurențiu Păncescu <lpancescu@gmail.com>
2016-09-08 18:30:37 +0200

    Minor fixes to the Vagrant test
    - 'vagrant ssh' requires -c or --command to run a specific command
    - prevent 'vagrant destroy' from prompting for confirmation

--- vagrant/tests/_test.sh
+++ vagrant/tests/_test.sh
@@ -5,6 +5,6 @@ for x in 6 7;do
   mkdir ${basedir}/${x} && cd ${basedir}/${x}
   vagrant init centos/${x}
   vagrant up --provider libvirt
-  vagrant ssh - "rpm -qa --last | head -n1 && uname -a"
-  vagrant destroy
+  vagrant ssh --command "rpm -qa --last | head -n1 && uname -a"
+  vagrant destroy --force
 done
